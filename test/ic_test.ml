ceef440bd7b12eaeb37bdadb9495c5b34f1c03e6
Will M. Farr <farr@mit.edu>
2009-10-06 17:56:57 -0500

    Added make_{hot,cold}_spherical energy test.

--- test/ic_test.ml
+++ test/ic_test.ml
@@ -5,12 +5,22 @@ module Ic = Ics.Make(B)
 module A = Analysis.Make(B)
 module E = A.E
 
+let test_e maker n ke pe = 
+  let bs = maker n in 
+  let bke = E.total_kinetic_energy bs and 
+      bpe = E.total_potential_energy bs in 
+  let sigma = 1.0 /. (sqrt (float_of_int n)) in 
+  let cmp = cmp_float ~epsrel:(3.0*.sigma) ~epsabs:(3.0*.sigma) in 
+  assert_equal ~cmp:cmp ~printer:string_of_float bke ke;
+  assert_equal ~cmp:cmp ~printer:string_of_float bpe pe
+
 let test_plummer_energies () = 
-  let bs = Ic.make_plummer 1000 in 
-  let ke = E.total_kinetic_energy bs and 
-      pe = E.total_potential_energy bs in 
-  assert_equal ~cmp:(cmp_float ~epsrel:0.07) ke 0.25;
-  assert_equal ~cmp:(cmp_float ~epsrel:0.07) pe (-0.5)
+  test_e Ic.make_plummer 1000 0.25 (-0.5)
+
+let test_spherical_energies () = 
+  test_e Ic.make_cold_spherical 1000 0.0 (-0.25);
+  test_e Ic.make_hot_spherical 1000 0.25 (-0.5)
 
 let tests = "ic.ml tests" >:::
-  ["plummer model energy test" >:: test_plummer_energies]
+  ["plummer model energy test" >:: test_plummer_energies;
+   "make_{hot,cold}_spherical energy test" >:: test_spherical_energies]
