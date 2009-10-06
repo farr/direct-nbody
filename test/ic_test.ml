76ef9716f9dac7a26d768552f7ba733b98d891f7
Will M. Farr <farr@mit.edu>
2009-10-06 17:40:27 -0500

    Made plummer energy test more stringent.

--- test/ic_test.ml
+++ test/ic_test.ml
@@ -9,8 +9,8 @@ let test_plummer_energies () =
   let bs = Ic.make_plummer 1000 in 
   let ke = E.total_kinetic_energy bs and 
       pe = E.total_potential_energy bs in 
-  assert_equal ~cmp:(cmp_float ~epsrel:0.1) ke 0.25;
-  assert_equal ~cmp:(cmp_float ~epsrel:0.1) pe (-0.5)
+  assert_equal ~cmp:(cmp_float ~epsrel:0.07) ke 0.25;
+  assert_equal ~cmp:(cmp_float ~epsrel:0.07) pe (-0.5)
 
 let tests = "ic.ml tests" >:::
   ["plummer model energy test" >:: test_plummer_energies]
