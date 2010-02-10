35764b283ecf5a47e0ccab4258c673ec1658b1c2
Will M. Farr <w-farr@northwestern.edu>
2010-02-10 13:17:51 -0600

    Back to unsafe in myocamlbuild.ml

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -6,7 +6,7 @@ let _ = dispatch begin function
   | After_rules -> 
       ocaml_lib ~extern:true ~dir:oUnit_dir "oUnit";
       flag ["native"; "compile"]
-        (S[A"-inline"; A"1000"]);
+        (S[A"-inline"; A"1000"; A"-unsafe"]);
       flag ["native"; "link"]
         (S[A"-inline"; A"1000"])
   | _ -> ()
