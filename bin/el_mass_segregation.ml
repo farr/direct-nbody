0d1c27a9537f782c26b80eff9a706314716e6a90
Will M. Farr <w-farr@northwestern.edu>
2010-12-13 16:37:41 -0500

    Continuous spectrum option in el_mass_segregation.

--- bin/el_mass_segregation.ml
+++ bin/el_mass_segregation.ml
@@ -12,28 +12,41 @@ let cpfile = ref "checkpoint.dat"
 let sumfile = ref "summary.dat"
 let heavyfrac = ref 0.01
 let heavyfac = ref 10.0
+let massspec = ref false
+let alpha = ref (-2.3) (* Salpeter IMF. *)
+let mmin = ref 0.1
+let mmax = ref 10.0
 let sf = ref 1e-4
 
 let options = 
-  [("-seed", Arg.Int (fun i -> Random.init i), "set the random seed");
-   ("-n", Arg.Set_int n, 
-    sprintf "number of bodies (default %d)" !n);
-   ("-dtcheck", Arg.Set_float dtcheck,
-    sprintf "time between checkpoint and data output (default %g)" !dtcheck);
-   ("-tevolve", Arg.Set_float tevolve,
-    sprintf "total evolution time (default %g)" !tevolve);
-   ("-statesfile", Arg.Set_string states_file,
-    sprintf "name of state file (default %s)" !states_file);
-   ("-cpfile", Arg.Set_string cpfile,
-    sprintf "name of checkpoint file (default %s)" !cpfile);
-   ("-sumfile", Arg.Set_string sumfile,
-    sprintf "name of summary file (default %s)" !sumfile);
-   ("-heavyfrac", Arg.Set_float heavyfrac,
-    sprintf "fraction of bodies that are heavy (default %g)" !heavyfrac);
-   ("-heavyfac", Arg.Set_float heavyfac,
-    sprintf "factor of increase in mass for heavy bodies (default %g)" !heavyfac);
-   ("-sf", Arg.Set_float sf,
-    sprintf "safety factor for integrator timestep (default %g)" !sf)]
+  Arg.align
+    [("-seed", Arg.Int (fun i -> Random.init i), "seed set the random seed");
+     ("-n", Arg.Set_int n, 
+      sprintf "n number of bodies (default %d)" !n);
+     ("-dtcheck", Arg.Set_float dtcheck,
+      sprintf "dt time between checkpoint and data output (default %g)" !dtcheck);
+     ("-tevolve", Arg.Set_float tevolve,
+      sprintf "t total evolution time (default %g)" !tevolve);
+     ("-statesfile", Arg.Set_string states_file,
+      sprintf "file name of state file (default %s)" !states_file);
+     ("-cpfile", Arg.Set_string cpfile,
+      sprintf "file name of checkpoint file (default %s)" !cpfile);
+     ("-sumfile", Arg.Set_string sumfile,
+      sprintf "file name of summary file (default %s)" !sumfile);
+     ("-heavyfrac", Arg.Set_float heavyfrac,
+      sprintf "f fraction of bodies that are heavy (default %g)" !heavyfrac);
+     ("-heavyfac", Arg.Set_float heavyfac,
+      sprintf "f factor of increase in mass for heavy bodies (default %g)" !heavyfac);
+     ("-massspec", Arg.Set massspec,
+      sprintf " flag for mass spectrum (default %b)" !massspec);
+     ("-alpha", Arg.Set_float alpha,
+      sprintf "alpha power-law exponent for mass spectrum (default %g)" !alpha);
+     ("-mmin", Arg.Set_float mmin,
+      sprintf "mmin minimum mass for spectrum (default %g)" !mmin);
+     ("-mmax", Arg.Set_float mmax,
+      sprintf "mmax maximum mass for spectrum (default %g)" !mmax);
+     ("-sf", Arg.Set_float sf,
+      sprintf "sf safety factor for integrator timestep (default %g)" !sf)]
 
 (* Should happen before we parse args *)
 let _ = 
@@ -61,7 +74,6 @@ let summary bs =
       (A.lagrange_radius bs 0.5)
       (A.lagrange_radius bs 0.75)
       (A.lagrange_radius bs 0.9);
-    flush out;
     close_out out;
     bs
     
@@ -69,7 +81,6 @@ let checkpoint bs =
   let cp_tmpname = !cpfile ^ ".tmp" in
   let cp_tmp = open_out_bin cp_tmpname in 
     Marshal.to_channel cp_tmp bs [];
-    flush cp_tmp;
     close_out cp_tmp;
     Unix.rename cp_tmpname !cpfile;
     bs
@@ -77,7 +88,6 @@ let checkpoint bs =
 let write_states bs = 
   let out = open_out_gen [Open_wronly; Open_append; Open_creat; Open_binary] 0o644 !states_file in
     Marshal.to_channel out bs [];
-    flush out;
     close_out out;
     bs
 
@@ -106,17 +116,33 @@ let write_el bs =
       close_out light;
       close_out heavy;
       bs
-      
 
 let filter bs = 
   checkpoint (write_states (write_el (summary bs)))
+
+let draw_mass_cont () = 
+  let max = max ((!mmin)**(!alpha)) ((!mmax)**(!alpha)) in 
+  let rec loop () = 
+    let x = !mmin +. (Random.float (!mmax -. !mmin)) and 
+        y = Random.float max in 
+      if x**(!alpha) < y then 
+        x
+      else
+        loop () in 
+    loop ()
+
+let draw_mass_disc () = 
+  if Random.float 1.0 < !heavyfrac then 
+    !heavyfac
+  else
+    1.0
 
 let _ = 
   Arg.parse options (fun _ -> ()) "el_mass_segregation.native OPTIONS";
   let bs = 
     Ic.rescale_to_standard_units 
       (Ic.adjust_frame 
-         (Ic.add_mass_spectrum (fun _ -> if Random.float 1.0 < !heavyfrac then !heavyfac else 1.0)
+         (Ic.add_mass_spectrum (fun _ -> if !massspec then draw_mass_cont () else draw_mass_disc ())
             (Ic.make_plummer !n))) in 
   let _ = filter bs in
   let rec advance_loop bs = 
